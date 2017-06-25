9e78ef273adb44f7bb9e7f99b8161ad3eea62eb7
Garland L. Riley Jr <Flex-Luthor@users.noreply.github.com>
2017-06-25 14:52:10 -0400

    Sidebar (#1)
    * adding mana cost to sidebar
    
    * card position changes and sidebar changes
    
    * made card section and header
    
    * more sidebar section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardSectionComponent } from './card-section/card-section.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    CardSectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
